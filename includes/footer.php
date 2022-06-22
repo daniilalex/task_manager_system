4ff5ee6b54bd8226991fa8a288289f08d9c1083e
danalex <daniil.alex1@gmail.com>
2022-06-22 10:35:31 +0300

    create log in page with includes and styles

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    &copy 2022
+</div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?
+include 'includes/bootstrap.php';
+
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $errors = [];
+
+    if (empty($email) || empty($password)) {
+        $errors[] = '<p style="color: red">There are empty spaces</p>';
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = '<p style="color: red">Wrong password</p>';
+    }
+    if (!preg_match('/[A-Za-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+        $errors[] = '<p style="color: red">Must be letters and numbers</p>';
+    }
+
+    if (empty($errors)) {
+        session_start();
+        $email = $_SESSION['email'];
+        header('Location: /pages/main.php');
+    }
+}
+?>
+
+
+
+    <div class="container">
+        <div class="content">
+            <h2 class="h-log">Employee Task Manager System </h2>
+            <form class="form" action="index.php" method="post">
+
+                <div class="row g-3 align-items-center">
+
+                    <input type="email" class="form-control" name="email" placeholder="Email">
+
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+
+                <br>
+                <input type="submit" class="btn btn-primary" value="Log in">
+                <? if (isset($errors)) {
+
+                    foreach ($errors as $error) { ?>
+                        <div>
+                            <?php echo $error ?>
+                        </div>
+                    <? }
+                } ?>
+            </form>
+        </div>
+    </div>
+
+<?php include 'includes/footer.php';
+
