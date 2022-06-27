c0017652951266aac39670f52d01d85b7b494eb0
danalex <daniil.alex1@gmail.com>
2022-06-27 20:16:04 +0300

    updated

--- Classes/Repository.php
+++ Classes/Repository.php
@@ -67,24 +67,7 @@ class Repository
         return fpassthru($f);
 
     }
-
-    function setPagination()
-    {
-        global $wp_query;
-        $links = $this->paginate_links(array(
-            'current' => max(1, get_query_var('paged')),
-            'total' => $wp_query->max_num_pages,
-            'type' => 'list',
-            'mid_size' => 3,
-        ));
-        $links = str_replace('page-numbers', 'pgn__num', $links);
-        $links = str_replace("<ul class='pgn__num'>", "<ul>", $links);
-        $links = str_replace('next pgn__num', 'pgn__next', $links);
-        $links = str_replace('prev pgn__num', 'pgn__prev', $links);
-        echo wp_kses_post($links);
-    }
-
-
+    
 }
 
 
